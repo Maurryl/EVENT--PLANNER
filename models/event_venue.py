567df858a464f44b21e32cd1ba3efdfe75857d1f
Parakeet <146772910+Maurryl@users.noreply.github.com>
2024-06-16 22:57:45 +0300

    Update event_venue.py

--- models/event_venue.py
+++ models/event_venue.py
@@ -1,45 +1,16 @@
-from sqlalchemy import Column, Integer, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-
-
-from models.base import Base
-from datetime import datetime
-
-Base = declarative_base()
-
-DATABASE_URL = 'sqlite:///event_planner.db'
-engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
-session = Session()
-
+from . import Base
 
 class EventVenue(Base):
-    __tablename__ = "event_venues"
+    __tablename__ = 'event_venues'
 
-    event_id = Column(Integer, ForeignKey("events.id"), primary_key=True)
-    venue_id = Column(Integer, ForeignKey("venues.id"), primary_key=True)
-
-    created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    event_id = Column(Integer, ForeignKey('events.id'))
+    venue_id = Column(Integer, ForeignKey('venues.id'))
 
     event = relationship("Event", back_populates="event_venues")
-    venue = relationship("Venue")
+    venue = relationship("Venue", back_populates="event_venues")
 
-    # def __repr__(self):
-    #     return f"<EventVenue(event_id={self.event_id}, venue_id={self.venue_id})>"
-    
-
-    @classmethod
-    def assign_venue_to_event(cls, event_id, venue_id):
-        event_venue = cls(event_id=event_id, venue_id=venue_id)
-        session.add(event_venue)
-        session.commit()
-        return event_venue
-
-    @classmethod
-    def get_all(cls):
-        return session.query(cls).all()
+    def __repr__(self):
+        return f"<EventVenue(id={self.id}, event_id={self.event_id}, venue_id={self.venue_id})>"
