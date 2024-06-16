0b2e6409758572dc0d6fbb8a343928df9934a2d6
Parakeet <146772910+Maurryl@users.noreply.github.com>
2024-06-16 22:51:24 +0300

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,15 +1,2 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from .models import Base
-
-# SQLite database (you can change the path)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./event_management.db"
-
-# Create a SQLAlchemy engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-
-# Create all database tables (if they do not exist)
-Base.metadata.create_all(bind=engine)
-
-# Create a session factory
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#database/init.py
+from .connection import SessionLocal
